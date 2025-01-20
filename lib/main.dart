9fc7776cd6957612a4a902560e8b86f8999c2d18
Reiikuzoo <ryfaryt02@gmail.com>
2025-01-20 12:05:45 +0700

    first

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Aplikasi data siswa'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+ 
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: const InputDecoration(hintText: "First Name"),
+            )
+      ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: const InputDecoration(hintText: "Last Name"),
+            )
+      ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: const InputDecoration(hintText: "Classes"),
+            )
+      ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: const InputDecoration(hintText: "Major"),
+            )
+      ),
+       ElevatedButton(
+                    onPressed: () {
+                      
+                    },
+                    child: const Text('Kirim'),
+       ),
+       
+        ],
+      ),
+    
+    );
+  }
+}
